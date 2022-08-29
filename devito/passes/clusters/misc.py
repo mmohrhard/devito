8bd11c52ea64b3f30901f2d095b37bc9a33c9d50
Tim Burgess <timb@dugeo.com>
2022-08-29 12:40:51 +0800

    Revert "compiler: Patch fission (issue 1921)"
    This reverts commit 967c69f1993204b5d1216cec7e0567f943d3499c.

--- devito/passes/clusters/misc.py
+++ devito/passes/clusters/misc.py
@@ -301,14 +301,7 @@ class Fission(Queue):
         for (it, guards), g in groupby(clusters, key=lambda c: self._key(c, prefix)):
             group = list(g)
 
-            try:
-                test0 = any(SEQUENTIAL in c.properties[it.dim] for c in group)
-            except AttributeError:
-                # `it` is None because `c`'s IterationSpace has no `d` Dimension,
-                # hence `key = (it, guards) = (None, guards)`
-                test0 = True
-
-            if test0 or guards:
+            if any(SEQUENTIAL in c.properties[it.dim] for c in group) or guards:
                 # Heuristic: no gain from fissioning if unable to ultimately
                 # increase the number of collapsable iteration spaces, hence give up
                 processed.extend(group)
