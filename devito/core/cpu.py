bf0f26fe7f7c795b755fca51777e773c8aa3caea
Justin Wake <justinw@dug.com>
2023-06-13 09:46:38 +0800

    Best not to break CPU evaluation..

--- devito/core/cpu.py
+++ devito/core/cpu.py
@@ -130,6 +130,7 @@ class Cpu64OperatorMixin(object):
         # Recognised but unused by the CPU backend
         oo.pop('par-disabled', None)
         oo.pop('gpu-fit', None)
+        oo.pop('gpu-nofit', None)
 
         if oo:
             raise InvalidOperator("Unrecognized optimization options: [%s]"
