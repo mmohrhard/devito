61f54d11ab9787b7222332b7d38b62f00ad68341
Justin Wake <justinw@dug.com>
2023-09-26 21:32:06 +0800

    Remove now-unused reference to CudaCallable

--- devito/passes/iet/engine.py
+++ devito/passes/iet/engine.py
@@ -1,7 +1,7 @@
 from collections import Counter, OrderedDict, namedtuple
 from functools import partial, wraps
 
-from devito.ir.iet import (Call, CudaCallable, FindNodes, FindSymbols, MetaCall, Transformer,
+from devito.ir.iet import (Call, FindNodes, FindSymbols, MetaCall, Transformer,
                            EntryFunction, ThreadCallable, Uxreplace,
                            derive_parameters)
 from devito.tools import DAG, as_tuple, filter_ordered, timed_pass
